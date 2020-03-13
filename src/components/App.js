be98a4ab33367f20d8aa0aa3001accd58c9fa205
wenglin1 <wenglin@buffalo.edu>
2020-03-13 12:44:08 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,52 @@ import React, { Component } from "react";
 import "../App.css";
 import Nav from "./Nav";
 import hogs from "../porkers_data";
-import HelloWorld from "./HelloWorld";
+import PigPen from "./PigPen";
+import Dropdown from "./Dropdown";
 
 class App extends Component {
+
+  state={
+    piggies: [],
+    filterTerm: "Greased"
+  }
+
+  componentDidMount(){
+    this.setState({
+      piggies: hogs
+    })
+  }
+
+  changeFilterTerm = (termFromChild) => {
+    this.setState({
+      filterTerm: termFromChild
+    })
+  }
+
+  returnAnArray = () => {
+    let theArrayToReturn = this.state.piggies
+    if (this.state.filterTerm === "Greased") {
+      theArrayToReturn = this.state.piggies.filter((pig) => {
+        return pig.greased
+      })
+    }
+    if (this.state.filterTerm === "Clean") {
+      theArrayToReturn = this.state.piggies.filter((pig) => {
+        return !pig.greased
+      })
+    } 
+    return theArrayToReturn
+  }
+
   render() {
     return (
       <div className="App">
         <Nav />
-        <HelloWorld />
+        <Dropdown 
+          filterTerm={this.state.filterTerm} 
+          changeFilterTerm={this.changeFilterTerm}
+        />
+        <PigPen piggies={this.returnAnArray()}/>
       </div>
     );
   }
